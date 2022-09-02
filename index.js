9abffad949f1c3e42319de834bae47d4cd6ec91a
nagamanikanta amudalapalli <58429976+NAGAMANIKANTA001@users.noreply.github.com>
2022-09-02 05:59:39 +0000

    Completed Challenge

--- index.js
+++ index.js
@@ -5,10 +5,16 @@ const cors = require('cors');
 const bodyparser = require('body-parser')
 const dns = require('dns')
 const shortId = require('shortid')
-const random = require('random')
 const app = express();
-
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const mySecret = process.env['MONGO_URI']
+console.log(mySecret)
+mongoose.connect(mySecret, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+  if (err) {
+    console.log(err.message)
+  } else {
+    console.log("Connected")
+  }
+});
 const { stringify } = require('querystring');
 const urlSchema = new mongoose.Schema({
   url:String,
@@ -34,31 +40,54 @@ app.get('/api/shorturl/:su', (req, res) => {
   console.log(req.params.su)
   URL.findOne({short_url:req.params.su},(err,data)=> {
     if(err) {
+      console.log("Not Found")
       res.send("Not Found")
     } else if(data != null){
+      console.log(data.url)
       res.redirect(data.url)
     } else {
+      console.log("Not Found")
       res.send("Not Found")
     }
   })
 })
 app.post('/api/shorturl', (req, res) => {
+  
+  // This is just for testing purposes
   console.log(req.body)
+  console.log(req.get('host'))
   const url=req.body.url.replace(/(^\w+:|^)\/\//, '');
+  if(url.indexOf(req.get('host'))!=-1) {
+    let newURL = new URL({url:req.body.url})
+      newURL.save((err,data)=>{
+        if(err) {
+          console.log("Data Not Saved")
+          res.send("Data not saved")
+        } else {
+          console.log({ original_url: req.body.url, short_url: data.short_url })
+          res.json({ original_url: req.body.url, short_url: data.short_url })
+        }
+      })
+  }
+  else {
   dns.lookup(url, (err,address,family) => {
     if (err) {
-      res.json({ error: "inavlid url" })
+      console.log("Invalid URL")
+      res.json({ error: "invalid url" })
     } else {
       let newURL = new URL({url:req.body.url})
       newURL.save((err,data)=>{
         if(err) {
+          console.log("Data Not Saved")
           res.send("Data not saved")
         } else {
+          console.log({ original_url: req.body.url, short_url: data.short_url })
           res.json({ original_url: req.body.url, short_url: data.short_url })
         }
       })
     }
   })
+  }
 })
 
 // Your first API endpoint
